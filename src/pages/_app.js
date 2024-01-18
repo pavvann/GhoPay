45d89810471425eed9a7126594cbcdc1e0aa54bb
Pawan <pawanbajaj2012@gmail.com>
2024-01-18 10:36:12 +0530

    ConnectKit enabled

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,29 @@
 import '@/styles/globals.css'
 
+import { WagmiConfig, createConfig, sepolia } from "wagmi";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    chains: [sepolia],
+    alchemyId: process.env.ALCHEMY_ID, // or infuraId
+    walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID,
+
+    appName: "GhoPay",
+
+    appDescription: "Payments, anyway you want.",
+
+  }),
+);
+
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider theme="retro">
+        <Component {...pageProps} />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  )
 }
